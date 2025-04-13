4c761749ca7a1fb8e99de328ec056f19cb7f8617
hemxanthh <hemanthkeerthi565@gmail.com>
2025-04-13 10:58:35 +0530

    Task 2!

--- top 3 cusines.py
+++
@@ -1,5 +0,0 @@
-# Split the cuisines into individual entries and count their occurrences
-cuisine_counts = df['Cuisines'].dropna().str.split(', ').explode().value_counts()
-
-# Display the top 10 most common cuisines
-print("Top 10 Most Common Cuisines:\n", cuisine_counts.head(10))
